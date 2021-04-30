ad37b79e7dffdf799c5a94eaece2fc2a92e9dc9f
HgDcMinh12 <81080107+HgDcMinh12@users.noreply.github.com>
2021-04-30 22:43:44 +0700

    Update getDataTool.py
    get last file name in exist folder to set the new file's name

--- getDataTool.py
+++ getDataTool.py
@@ -1,11 +1,12 @@
 import cv2
 import os
+import glob
 
 cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("test")
-
-img_counter = 0
+cv2.resizeWindow('test',1920,1080)
+img_counter = 1
 
 while True:
     ret, frame = cam.read()
@@ -13,18 +14,26 @@ while True:
     frame = cv2.rectangle(frame, (319, 49), (621, 351), (0, 255, 0), 1)
     cv2.imshow("test", frame)
     img = frame[50:351, 320:621]
-    k = cv2.waitKey()
+    k = cv2.waitKey(1)
     keyname = chr(k%256)
-    path = "D:/UIT/HKII_2020_2021/Nhan dang/getDataTool/Data/{}".format(keyname)
+    path = "./Data/{}".format(keyname)
     if k%256 == 27:
         # ESC pressed
         print("Escape hit, closing...")
         break
+    elif k == -1:
+        continue
     else:
         if os.path.isdir(path):
+            if os.listdir(path):
+                list_of_files = glob.glob(path+'/*')
+                latest_file = max(list_of_files, key = os.path.getctime)
+                img_counter = int(latest_file.split('.')[1].rsplit('\\')[1][1:])+1
+                # print(img_counter)
+
             img_name = "{}/{}{}.png".format(path, keyname, img_counter)
             cv2.imwrite(img_name, img)
-            print("{} written!".format(img_name))
+            print("{} written!".format(img_counter))
             img_counter += 1
         else:
             os.mkdir(path)
